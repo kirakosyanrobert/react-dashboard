08b71b33b237b23b361b3f35eefa336e8e325926
Robert <kirakosyanrobert2018@gmail.com>
2020-04-06 03:44:35 +0400

    create Org fields

--- src/components/forms/CreateOrganizationForm/CreateOrganizationForm.jsx
+++ src/components/forms/CreateOrganizationForm/CreateOrganizationForm.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Form, InputGroup, FormControl, Badge } from 'react-bootstrap';
+import { Form, InputGroup, FormControl, Badge, Col, ListGroup } from 'react-bootstrap';
 import { v4 as uuid } from 'uuid';
 
 import './CreateOrganizationForm.scss';
-import { Button, ButtonVariants, ButtonActionTypes } from '../../ui/Button';
+import { Button, ButtonVariants, ButtonActionTypes, ButtonSizes } from '../../ui/Button';
+import Modal from '../../ui/Modal/Modal';
 import { useTranslation } from '../../../hooks';
 import { MapView } from '../../maps/MapView/MapView';
 import { MapCard } from '../../maps/MapCard/MapCard';
@@ -61,42 +62,76 @@ function CreateOrganizationForm ({onCreate}) {
     const translate = useTranslation();
     
     const [title, setTitle] = useState('');
-    const [email, setEmail] = useState('');
-    const [website, setWebsite] = useState('');
+    const [description, setDescription] = useState('');
+
+    const [categories, setCategories] = useState([{
+        category: 'Medicine',
+        subCategory: 'Hospital'
+    }]);
+    const [openCategoriesModal, setOpenCategoriesModal] = useState(false);
+
+   
     const [address, setAddress] = useState('');
+    const [coordinates, setCoordinates] = useState({lat: '', lon: ''});
+    const [openMapView, setOpenMapView] = useState(false);
+
+
+    const [nearestStopTitle, setNearestStopTitle] = useState('');
+    const [nearestStopCoordinates, setNearestStopCoordinates] = useState({lat: '', lon: ''});
+    const [openNearestStopMap, setOpenNearestStopMap] = useState(false);
+
+
     const [postCode, setPostCode] = useState('');
     const [numberOfFloors, setNumberOfFloors] = useState('');
-    const [coordinates, setCoordinates] = useState({lat: '', lon: ''});
     const [phoneNumbers, setPhoneNumbers] = useState(['']);
     const [newTag, setNewTag] = useState('#');
     const [tags, setTags] = useState([]);
     const [workingHours, setWorkingHours] = useState([[], [], [], [], [], [], []]);
 
-    const [openMapView, setOpenMapView] = useState(false);
+    const [email, setEmail] = useState('');
+    const [website, setWebsite] = useState('');
+    const [note, setNote] = useState('');
+
+
 
 
 
     function handleSubmit (e) {
         e.preventDefault();
 
-        if(title && email && website && address && postCode && numberOfFloors && 
-            coordinates.lat && coordinates.lon) {
+        if(
+            title &&
+            tags.length > 0 &&
+            address && 
+            coordinates.lat && 
+            coordinates.lon &&
+            nearestStopCoordinates.lat &&
+            nearestStopCoordinates.lon &&
+            postCode && 
+            numberOfFloors
+        ) {
             const org = {
                 id: uuid(),
                 title,
-                email,
+                searchWords: tags,
                 address,
                 postCode: +postCode,
                 numberOfFloors: +numberOfFloors,
                 coordinates,
                 phoneNumbers,
                 tags,
-                workingHours
+                workingHours,
+                website,
+                email,
             };
-            onCreate(org);
+            // onCreate(org);
         } else {
             alert("Fill in Inputs");
         }
+    }
+
+    function handleRemoveCategory (categoryIndex) {
+        setCategories(categories.filter((cat, i) => i !== categoryIndex));
     }
 
     //Phone Number Actions
@@ -121,12 +156,21 @@ function CreateOrganizationForm ({onCreate}) {
     //Tags Actions
     function handleAddTag() {
         if(newTag.length > 1) {
+            if(newTag[0] !== '#') {
+                const modifiedNewTag = '#' + newTag;
+                setTags([modifiedNewTag, ...tags])
+                setNewTag('#');
+                return;
+            }
             setTags([newTag, ...tags])
             setNewTag('#');
         }
     }
 
-    // function handleRemoveTag() {}
+    function handleRemoveTag(tagIndex) {
+        setTags(tags.filter((t, index) => index !== tagIndex))
+    }
+
 
     function handleGetWorkingHours (index, place, value) {
         const tempWorkingHours = [...workingHours];
@@ -138,20 +182,97 @@ function CreateOrganizationForm ({onCreate}) {
         setCoordinates({
             lon: coords[0],
             lat: coords[1]
-        })
+        });
+        setTimeout(() => setOpenMapView(false), 0);
     }
+
+    function handleGetNearestStopCoords (coords) {
+        setNearestStopCoordinates({
+            lon: coords[0],
+            lat: coords[1]
+        });
+        setTimeout(() => setOpenNearestStopMap(false), 0);
+    }
+
+    const Required = () =>  (
+        <span className="text-danger">*</span>
+    );
 
     return (
         <>
+        <Modal
+            title="Categories"
+            open={openCategoriesModal}
+            onClose={() => setOpenCategoriesModal(false)}
+        >
+            <div>
+                <ListGroup>
+                    {categories.length > 0 &&
+                        categories.map((item, index) => (
+                            <ListGroup.Item key={`categorie-edit-item-${index}`}>
+                                {`${item.category} - ${item.subCategory}`}
+                                <Button
+                                    className="float-right"
+                                    title="X"
+                                    size={ButtonSizes.Small}
+                                    variant={ButtonVariants.Danger}
+                                    onClick={() => handleRemoveCategory(index)}
+                                />
+                            </ListGroup.Item>
+                        ))
+                    }
+                </ListGroup>
+            </div>
+        </Modal>
         <Form onSubmit={handleSubmit}>
             <Form.Group>
-                <Form.Label>{translate(({inputs}) => inputs.title.title)}</Form.Label>
+                <Form.Label>
+                    {translate(({inputs}) => inputs.title.title)}
+                    <Required />
+                </Form.Label>
                 <Form.Control
+                    required
                     type="text"
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
                 />
             </Form.Group>
+
+            <Form.Group>
+                <Form.Label>
+                    {translate(({inputs}) => inputs.description.title)}
+                </Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows="3"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>
+                    {/* {translate(({inputs}) => inputs.description.title)} */}
+                    Categories
+                </Form.Label>
+                <ListGroup horizontal>
+                    {categories.length > 0 &&
+                        categories.map((item, index) => (
+                            <ListGroup.Item key={`categorie-item-${index}`}>
+                                {`${item.category} - ${item.subCategory}`}
+                            </ListGroup.Item>
+                        ))
+                    }
+                    <ListGroup.Item>
+                       <Button
+                            title={translate(({buttons}) => buttons.edit)}
+                            onClick={() => setOpenCategoriesModal(true)}
+                       />
+                    </ListGroup.Item>
+                </ListGroup>
+            </Form.Group>
+
+            
             <Form.Group>
                 <Form.Label>{translate(({inputs}) => inputs.email.title)}</Form.Label>
                 <Form.Control
@@ -168,14 +289,127 @@ function CreateOrganizationForm ({onCreate}) {
                     onChange={(e) => setWebsite(e.target.value)}
                 />
             </Form.Group>
-            <Form.Group>
-                <Form.Label>{translate(({inputs}) => inputs.address.title)}</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                />
-            </Form.Group>
+          
+
+            <Form.Row>
+                <Col lg={6}>
+                    <Form.Group>
+                        <Form.Label>
+                            {translate(({inputs}) => inputs.address.title)}
+                            <Required />
+                        </Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                    </Form.Group>
+                </Col>
+                  
+                <Col lg={6}>
+                    <Form.Row>
+                        <Form.Group as={Col}>
+                            <Form.Label>
+                                {translate(({inputs}) => inputs.latitude.title)}
+                                <Required />
+                            </Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={coordinates.lat}
+                                onChange={(e) => setCoordinates({...coordinates, lat: e.target.value})}
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Label>
+                                {translate(({inputs}) => inputs.longitude.title)}
+                                <Required />
+                            </Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={coordinates.lon}
+                                onChange={(e) => setCoordinates({...coordinates, lon: e.target.value})}
+                            />
+                        </Form.Group>
+                        {/* <MapCard lon={coordinates.lon} lat={coordinates.lat} /> */}
+                        <div className="d-flex align-items-end mb-3">
+                            <Button
+                                variant={ButtonVariants.Primary}
+                                title={'Show on map'}
+                                disabled={openMapView}
+                                onClick={() => setOpenMapView(true)}
+                            />
+                        </div>
+                    </Form.Row>
+                </Col>
+            </Form.Row>
+            {openMapView &&
+                <div
+                    className="mb-4">
+                    <MapView getCoords={handleGetCoords} />
+                </div>
+            }
+
+
+
+
+        <Form.Row>
+            <Col lg={6}>
+                <Form.Group>
+                    <Form.Label>
+                        {translate(({inputs}) => inputs.nearestStop.title)}
+                        <Required />
+                    </Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={nearestStopTitle}
+                        onChange={(e) => setNearestStopTitle(e.target.value)}
+                    />
+                </Form.Group>
+            </Col>
+                
+            <Col lg={6}>
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Label>
+                            {translate(({inputs}) => inputs.latitude.title)}
+                            <Required />
+                        </Form.Label>
+                        <Form.Control
+                            type="number"
+                            value={nearestStopCoordinates.lat}
+                            onChange={(e) => setNearestStopCoordinates({...nearestStopCoordinates, lat: e.target.value})}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>
+                            {translate(({inputs}) => inputs.longitude.title)}
+                            <Required />
+                        </Form.Label>
+                        <Form.Control
+                            type="number"
+                            value={nearestStopCoordinates.lon}
+                            onChange={(e) => setNearestStopCoordinates({...nearestStopCoordinates, lon: e.target.value})}
+                        />
+                    </Form.Group>
+                    <div className="d-flex align-items-end mb-3">
+                        <Button
+                            variant={ButtonVariants.Primary}
+                            title={'Show on map'}
+                            disabled={openNearestStopMap}
+                            onClick={() => setOpenNearestStopMap(true)}
+                        />
+                    </div>
+                </Form.Row>
+            </Col>
+        </Form.Row>
+        {openNearestStopMap &&
+            <div
+                className="mb-4">
+                <MapView getCoords={handleGetNearestStopCoords} />
+            </div>
+        }
+
+
             <Form.Group>
                 <Form.Label>{translate(({inputs}) => inputs.postCode.title)}</Form.Label>
                 <Form.Control
@@ -194,41 +428,14 @@ function CreateOrganizationForm ({onCreate}) {
             </Form.Group>
 
 
-            <Form.Group>
-                <Form.Label>{translate(({inputs}) => inputs.latitude.title)}</Form.Label>
-                <Form.Control
-                    type="number"
-                    value={coordinates.lat}
-                    onChange={(e) => setCoordinates({...coordinates, lat: e.target.value})}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>{translate(({inputs}) => inputs.longitude.title)}</Form.Label>
-                <Form.Control
-                    type="number"
-                    value={coordinates.lon}
-                    onChange={(e) => setCoordinates({...coordinates, lon: e.target.value})}
-                />
-            </Form.Group>
 
-            <MapCard lon={coordinates.lon} lat={coordinates.lat} />
-            <Button
-                className="mb-4"
-                variant={ButtonVariants.Primary}
-                title={openMapView ? 'Hide map' : 'Show on map'}
-                onClick={() => setOpenMapView(!openMapView)}
-            />
-            {openMapView &&
-                <div
-                    className="mb-4">
-                    <MapView getCoords={handleGetCoords} />
-                </div>
-            }
 
 
             {phoneNumbers.map((item, index) => (
                 <Form.Group key={`phone-number-input-${index}`}>
-                    <Form.Label>{`${translate(({inputs}) => inputs.phoneNumber.title)} ${index + 1}`}</Form.Label>
+                    <Form.Label>
+                        {`${translate(({inputs}) => inputs.phoneNumber.title)} ${index + 1}`}
+                    </Form.Label>
                     <InputGroup className="mb-3">
                         <FormControl
                             type="text"
@@ -256,7 +463,10 @@ function CreateOrganizationForm ({onCreate}) {
 
 
             <Form.Group>
-                <Form.Label>{translate(({inputs}) => inputs.tags.title)}</Form.Label>
+                <Form.Label>
+                    {translate(({inputs}) => inputs.tags.title)}
+                    <Required />
+                </Form.Label>
                 <InputGroup className="mb-3">
                     <Form.Control 
                         type="text"
@@ -273,9 +483,20 @@ function CreateOrganizationForm ({onCreate}) {
                 </InputGroup>
                 <div className="w-100 d-flex flex-wrap">
                     {tags.map((tag, index) => (
-                         <h4 key={`tag-item-${index}`} className="mr-3">
-                            <Badge variant="secondary">{tag}</Badge>
-                        </h4>
+                        <div
+                            key={`tag-item-${index}`}
+                            className="mr-3 bg-light d-flex align-items-center"
+                        >
+                            <h4>
+                                <Badge variant="secondary">{tag}</Badge>
+                            </h4>
+                            <h5
+                                className="px-2 cursor-pointer"
+                                onClick={() => handleRemoveTag(index)}
+                            >
+                                X
+                            </h5>
+                        </div>
                     ))}
                 </div>
             </Form.Group>
@@ -315,6 +536,18 @@ function CreateOrganizationForm ({onCreate}) {
                         </div>
                 ))}
             </div>
+
+            <Form.Group>
+                <Form.Label>
+                    {translate(({inputs}) => inputs.note.title)}
+                </Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows="3"
+                    value={note}
+                    onChange={(e) => setNote(e.target.value)}
+                />
+            </Form.Group>
 
 
             <Button
